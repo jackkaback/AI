b280c5c4c6f5113269af179fbfc4c81c89f7b564
jack <jackkaback@gmail.com>
2017-12-31 18:39:08 -0500

    random bag

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <time.h>
+#include <stdlib.h>
 
 typedef struct bag{
 	bool items [24];
@@ -10,6 +12,20 @@ typedef struct bag{
 
 
 int main() {
+	srand(time(NULL));
+	struct bag bags;
+	for (int ii = 0; ii < 24; ii++){
+		int r = rand();
+
+		if (r % 2){
+			bags.items[ii] = true;
+		}
+		else{
+			bags.items[ii] = false;
+		}
+	}
+
+
 	printf("Hello, World!\n");
 	return 0;
 }
